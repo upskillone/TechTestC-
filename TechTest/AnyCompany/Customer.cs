7f6d52ed90c75435e1d11fcba31b9502543a4eaf
UK\OSotorrio <oscar.sotorrio@investec.co.uk>
2018-04-12 10:57:35 +0100

    Added exercise code

---
+++ TechTest/AnyCompany/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AnyCompany
+{
+    public class Customer
+    {
+        public string Country { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ TechTest/AnyCompany/OrderService.cs
@@ -0,0 +1,24 @@
+﻿namespace AnyCompany
+{
+    public class OrderService
+    {
+        private readonly OrderRepository orderRepository = new OrderRepository();
+
+        public bool PlaceOrder(Order order, int customerId)
+        {
+            Customer customer = CustomerRepository.Load(customerId);
+
+            if (order.Amount == 0)
+                return false;
+
+            if (customer.Country == "UK")
+                order.VAT = 0.2d;
+            else
+                order.VAT = 0;
+
+            orderRepository.Save(order);
+
+            return true;
+        }
+    }
+}
